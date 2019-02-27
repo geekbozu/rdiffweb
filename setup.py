93b3d88fa8a0d4913e75e6ae08f5c733b9e0ee78
Patrik Dufresne <info@patrikdufresne.com>
2019-02-27 13:08:58 -0500

    Fix build
    * Stop using sitepackages
    * add coverage as dependency
    * fix dependency for cherrypy p2.7 (v18 doesn't support python2.7)

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ class build(build_):
 
 # Compute requirements
 install_requires = [
-    "CherryPy>=3.2.2",
+    "CherryPy>=3.2.2,<17" if PY2 else "CherryPy>=3.3.2",
     "Jinja2>=2.6,<=2.8.1",
     "future>=0.15.2",
     "psutil>=2.1.1",
